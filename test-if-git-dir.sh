9fcbd27bfd49183f50b153cf94702240a58b4c93
Kelly Towle <kelly.towle@substantial.com>
2022-10-16 22:05:28 -0700

    Add test-if-git-dir.sh

---
+++ test-if-git-dir.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+inside_git_repo="$(git rev-parse --is-inside-work-tree 2> /dev/null)"
+
+source $SCRIPTS_PATH/colors.sh
+
+if [ "$inside_git_repo" ]; then
+  # inside git repo
+  exit 0
+else
+  echo -e "Not a git repo directory: ${BRed}$(pwd)${RCol}"
+  exit 1
+fi
